eb3c54075f9b9c76e13d5e7c8ef3d1e02d6cbf22
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-08 22:25:40 -0500

    Figured a way around returning stack memory from a function so no need to use dynamic memory:Complete

--- merge_sort/C++/merge_sort.cpp
+++ merge_sort/C++/merge_sort.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int* merge_sort(int *array, int arr_len);
-int* merge(int *left, int *right, int *sorted_arr, int l_len, int r_len);
+int* merge(int *left, int *right, int *container, int l_len, int r_len);
 void slice(int *array,int *new_array, int start, int end);
 void print(int *array, int len);
 
@@ -14,18 +14,18 @@ int main() {
 
 	int arr[LEN] = {56, 2, 32, 1, 0, 2, 0, 19, 67, 45, 3, 2, 6, 4, 3, 13, 5, 43, 5, 12};
 
-	int *sorted = merge_sort(arr, LEN);
+	merge_sort(arr, LEN);
 
-	print(sorted, LEN);
+	print(arr, LEN);
 
 }
 
 int* merge_sort(int *array, int arr_len) {
 
 	// If array only has 1 element, done splitting.
-	if(arr_len < 2) {
+	if(arr_len < 2) 
 		return array;
-	}
+
 	// Middle index
 	int mid_indx = arr_len/2,
 	 	l_len = mid_indx,
@@ -40,48 +40,56 @@ int* merge_sort(int *array, int arr_len) {
 	int right[r_len];
 	slice(array, right, mid_indx, arr_len);
 
-	int *sorted_arr = new int[arr_len];
+	// To merge elements into
+	int container[arr_len];
 
 	// Merge ints in order for each half this function creates using reccursion.
-	merge(merge_sort(left, l_len), merge_sort(right, r_len), sorted_arr, l_len, r_len);
+	merge(merge_sort(left, l_len), merge_sort(right, r_len), container, l_len, r_len);
 
-	return sorted_arr;
+	// Replace elements in array with the container where elements were merged.
+	for(int i = 0; i < arr_len; i ++)
+		array[i] = container[i];
+	return array;
 
 }
-int* merge(int *left, int *right, int *sorted_arr, int l_len, int r_len) {
+int* merge(int *left, int *right, int *container, int l_len, int r_len) {
 	// Variables and sorted_arr array where left and right arrays will merge into in order.
 	int l_indx = 0,
 		r_indx = 0;
+
 
 	// Merge elements from left and right array into sorted_arr array in order.
 	while(l_indx < l_len || r_indx < r_len) {
 		// Fill sorted_arr array with remaining elements if we are done with either half.
 		if(l_indx == l_len) {
-			sorted_arr[l_indx + r_indx] = right[r_indx];
+			container[l_indx + r_indx] = right[r_indx];
 			r_indx ++;
 		}
 		else if(r_indx == r_len) {
-			sorted_arr[l_indx + r_indx] = left[l_indx];
+			container[l_indx + r_indx] = left[l_indx];
 			l_indx ++;
 		}
 		// Fill in order when we are done with neither.
 		else if(left[l_indx] < right[r_indx]) {
-			sorted_arr[l_indx + r_indx] = left[l_indx];
+			container[l_indx + r_indx] = left[l_indx];
 			l_indx ++;
 		}
 		else {
-			sorted_arr[l_indx + r_indx] = right[r_indx];
+			container[l_indx + r_indx] = right[r_indx];
 			r_indx ++;
 		}
 	}
-	return sorted_arr;
+	return container;
 
 }
+// To slice array for left and right
 void slice(int *array,int *new_array, int start, int end) {
 	int new_len = end - start;
 	for(int i = 0; i < new_len; i ++) 
 		new_array[i] = array[start + i];
 }
+
+// Utility to print array
 void print(int *array, int len) {
 	cout << '[';
 	for(int i = 0; i < len; i++)
