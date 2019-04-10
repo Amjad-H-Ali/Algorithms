3674fdb7ec8650f0aaff8dcb4d795027ca038e98
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-10 00:15:22 -0500

    Used recursive strategy to find maximum sum of hour glass

--- hour_glass/JS/hourGlass.js
+++ hour_glass/JS/hourGlass.js
@@ -34,6 +34,21 @@ const hourGlass3 = [
 ];
 
 
+const findBiggestHourGlass = (hG, r=0, c=0) => {
+	if (c > hG[0].length - 3 && r == hG.length - 3) return null;
+	else if(c > hG[0].length - 3) return findBiggestHourGlass(hG, ++r, 0);
 
+	const top = hG[r][c] + hG[r][c+1] + hG[r][c+2],
+	   middle = hG[r+1][c+1],
+	   bottom = hG[r+2][c] + hG[r+2][c+1] + hG[r+2][c+2];
+
+	const sum = top + middle + bottom;
+
+	return Math.max(findBiggestHourGlass(hG, r, ++c), sum);
+}
+
+console.log(findBiggestHourGlass(hourGlass1)); // 19
+console.log(findBiggestHourGlass(hourGlass2)); // 4
+console.log(findBiggestHourGlass(hourGlass3)); // 28
 
 
