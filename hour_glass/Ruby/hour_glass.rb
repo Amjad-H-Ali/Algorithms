4d28c514aad90a1c6dd47182108554b19be3bc5c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-13 19:40:56 -0500

    Function max hour glass returns hour glass with max sum

--- hour_glass/Ruby/hour_glass.rb
+++ hour_glass/Ruby/hour_glass.rb
@@ -29,3 +29,22 @@ hourGlass3 = [
 	[ 0,  0,  0, -2, 0, 0],
 	[ 0,  0,  1,  2, 4, 0]
 ]
+
+def max_hour_glass hG, row=0, col=0
+	if col > hG[0].length - 3 && row == hG.length - 3
+		return hG[row][col-1] 
+	elsif col > hG[0].length - 3
+		return max_hour_glass hG, row+=1, 0
+	else
+		top = hG[row][col] + hG[row][col+1] + hG[row][col+2]
+		mid =                hG[row+1][col+1]
+		bottom = hG[row+2][col] + hG[row+2][col+1] + hG[row+2][col+2]
+		sum = top + mid + bottom
+
+		[max_hour_glass(hG, row, col+=1), sum].max
+	end
+end
+
+p max_hour_glass hourGlass1
+p max_hour_glass hourGlass2
+p max_hour_glass hourGlass3
