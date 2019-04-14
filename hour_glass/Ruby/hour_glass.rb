302dd50d8e32c87bf87ab148aa297914148b67c3
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-13 19:43:59 -0500

    Added comments

--- hour_glass/Ruby/hour_glass.rb
+++ hour_glass/Ruby/hour_glass.rb
@@ -31,20 +31,25 @@ hourGlass3 = [
 ]
 
 def max_hour_glass hG, row=0, col=0
+	# If at last hour glass
 	if col > hG[0].length - 3 && row == hG.length - 3
-		return hG[row][col-1] 
+		return hG[row][col-1] #Guarenteed non max amount to return and break out of reccursion
+	# If at last column, move to next row.
 	elsif col > hG[0].length - 3
 		return max_hour_glass hG, row+=1, 0
 	else
+		# Hour glass in 2D array
 		top = hG[row][col] + hG[row][col+1] + hG[row][col+2]
 		mid =                hG[row+1][col+1]
 		bottom = hG[row+2][col] + hG[row+2][col+1] + hG[row+2][col+2]
+		# Sum of hour glass
 		sum = top + mid + bottom
 
+		# Return max hour glass sum using reccursion
 		[max_hour_glass(hG, row, col+=1), sum].max
 	end
 end
 
-p max_hour_glass hourGlass1
-p max_hour_glass hourGlass2
-p max_hour_glass hourGlass3
+p max_hour_glass hourGlass1 #19
+p max_hour_glass hourGlass2 #4
+p max_hour_glass hourGlass3 #28
