b30e39d63c1231a7db7bc16bab7b4d7fdd5da10a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-17 00:01:57 -0500

    Implemted max hour glass function that returns largest sum of an hour glass

--- hour_glass/C++/hour_glass.cpp
+++ hour_glass/C++/hour_glass.cpp
@@ -2,9 +2,10 @@
 
 using namespace std;
 
-
+int maxHourGlass(const int (*hG)[6], int height, int width, int row=0, int col=0);
 
 int main() {
+
 
 	cout << "Hour Glass in C++!" << endl;
 
@@ -40,4 +41,38 @@ int main() {
 		{ 0,  0,  1,  2, 4, 0}	
 	};
 
+
+	cout << maxHourGlass(hourGlass1, ROW, COL) << '\n'  // 19
+		<< maxHourGlass(hourGlass2, ROW, COL) << '\n'   // 4
+		<< maxHourGlass(hourGlass3, ROW, COL) << endl;  // 28
+
 }
+
+int maxHourGlass(const int (*hG)[6], int height, int width, int row, int col) {
+
+	// Return, when last hour glass is reached
+	if(col > width - 3 && row == height - 3)
+		return hG[row][col-1]; // Guaranteed non max becuase we can't return NULL and compare to int.
+	// Reset col and move to next row when col has gone out of bounds.
+	if(col > width - 3)
+		return maxHourGlass(hG, height, width, ++row, 0);
+
+	// Define an hour glass
+	int top = hG[row][col] + hG[row][col+1] + hG[row][col+2],
+	 middle = hG[row+1][col+1],
+	 bottom = hG[row+2][col] + hG[row+2][col+1] + hG[row+2][col+2];
+
+	// Sum of an hour glass
+	int total = top + middle + bottom;
+
+	// Use recursion to get other totals and compare.
+	int previous_total = maxHourGlass(hG, height, width, row, ++col);
+
+	return total > previous_total ? total : previous_total;
+
+};
+
+
+
+
+
