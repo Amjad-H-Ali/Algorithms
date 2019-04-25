fdf92054b8b8043663eee1daf2b45929cdac0517
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-25 17:01:59 -0500

    Implemented Adding One Algorithm in JS

--- adding_one/JS/addingOne.js
+++ adding_one/JS/addingOne.js
@@ -1,1 +1,29 @@
 console.log("Adding One in JS!");
+
+// "Increments" an array
+const addingOne = (array, indx)=> {
+	// Start from end of array
+	if(indx==null) indx = array.length - 1;
+
+	// Base for reccursion
+	// Insert 1 at the beginning of the array 1. (Adding digit)
+	if(indx < 0) {
+		array.unshift(1);
+		return array;
+	}
+
+	// Carry the one using reccursion.
+	if(array[indx] >= 9) {
+		array[indx] = 0;
+		return addingOne(array, --indx);
+	}
+	else
+		array[indx]++;
+	return array;
+}
+
+console.log(addingOne([1, 2, 3, 4])); // 1,235
+
+console.log(addingOne([1, 2, 9, 9])); // 1,300
+
+console.log(addingOne([9, 9, 9, 9])); // 10,000
