e226a2a81cf592843be147c47034d8817843b4a0
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-04-28 16:42:13 -0500

    Implemented Adding One in Ruby

--- adding_one/Ruby/adding_one.rb
+++ adding_one/Ruby/adding_one.rb
@@ -1,1 +1,36 @@
 p "Adding One With Ruby!"
+
+# Adds one to an array of integers
+def add_one array, indx=nil
+	# Initialize indx to last index in array
+	if indx == nil
+		indx = array.length - 1
+	end
+
+	# Exit for reccursion(Add a digit and Insert one to the beginning)
+	if indx < 0
+		array.insert(0,1)
+		return array
+	end
+
+	# Carry the one if digit is 9
+	if array[indx] >= 9
+		array[indx] = 0
+		indx-=1
+		return add_one array, indx
+	
+
+	# Else just increment the digit
+	else
+		array[indx] +=1
+	end
+
+	return array
+
+end
+
+p add_one [1,2,3,4] #1,235
+
+p add_one [9,9,9] #1,000
+
+p add_one [3,7,8,0,9,9] #378,100
