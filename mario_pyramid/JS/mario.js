f4c2a53e93935cb18813305b8dcf3c350bb9eef3
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-03 21:20:45 -0500

    Implemented Reverse Mario Pyramid

--- mario_pyramid/JS/mario.js
+++ mario_pyramid/JS/mario.js
@@ -2,11 +2,29 @@ console.log("Mario Pyramid With JS!");
 
 
 
-// Prints a Pyramid given a height
-const makePyramid = (height) => {
+// Prints a Pyramid given a size
+const makePyramid = (size) => {
 
-	for(let row = "#"; row.length <= height; row+='#') 
+	// Rows
+	for(let row = "#"; row.length <= size; row+='#') 
 		console.log(row);
 }
 
 makePyramid(10);
+
+
+
+//Prints Reverse Pyramid
+const makeReversePyramid = (size) => {
+
+	// Rows
+	for(let row = "#", spaces; row.length <= size; row+='#') {
+		// Spaces
+		for(spaces = ""; spaces.length <= (size - row.length); spaces+=' ');
+			
+		console.log(spaces + row);
+	}
+}
+
+
+makeReversePyramid(10);
