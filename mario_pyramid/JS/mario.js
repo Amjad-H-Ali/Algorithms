5958af66a447712a9ecca1ec671886150d40e506
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-03 19:59:42 -0500

    Implemented Mario Pyramid in JS

--- mario_pyramid/JS/mario.js
+++ mario_pyramid/JS/mario.js
@@ -1,1 +1,12 @@
 console.log("Mario Pyramid With JS!");
+
+
+
+// Prints a Pyramid given a height
+const makePyramid = (height) => {
+
+	for(let row = "#"; row.length <= height; row+='#') 
+		console.log(row);
+}
+
+makePyramid(10);
