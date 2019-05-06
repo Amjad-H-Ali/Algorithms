1bf3fd3ecf3d9b8132354f75469bdce2d51cdcd6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-05 19:38:41 -0500

    Implemented function that prints mario pyramid

--- mario_pyramid/Python/mario.py
+++ mario_pyramid/Python/mario.py
@@ -1,1 +1,12 @@
-print("Mario Pyramid in Python!");
+print("Mario Pyramid in Python!")
+
+# Prints rows given a size
+def mario_pyramid(size):
+
+	row = "#"
+
+	for block in range(size):
+		print(row)
+		row+="#"
+
+mario_pyramid(10)
