72a3b49d59df200caa491189523d79414cf04ccb
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-05 19:47:36 -0500

    Implemented Mario Pyramid in reverse

--- mario_pyramid/Python/mario.py
+++ mario_pyramid/Python/mario.py
@@ -1,12 +1,30 @@
 print("Mario Pyramid in Python!")
 
-# Prints rows given a size
+# Prints mario pyramid given a size
 def mario_pyramid(size):
 
 	row = "#"
 
+	# Prints row of blocks
 	for block in range(size):
 		print(row)
 		row+="#"
 
 mario_pyramid(10)
+
+# Prints mario pyramid in reverse
+def mario_reverse_pyramid(size):
+
+	row = "#"
+
+	# Prints row of Blocks
+	for block in range(size):
+		# Prints spaces
+		spaces = ""	
+		for space in range(size - len(row)):
+			spaces += " "
+			
+		print(spaces+row);
+		row += "#"
+
+mario_reverse_pyramid(10)
