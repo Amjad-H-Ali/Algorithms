93e377e0c7d56aeb17937a25be2ecfa51c52dced
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-06 19:20:44 -0500

    Implemented mario pyramid in ruby

--- mario_pyramid/Ruby/mario.rb
+++ mario_pyramid/Ruby/mario.rb
@@ -1,1 +1,16 @@
 puts "Mario Pyramid in Ruby!"
+
+# Prints Mario Pyramid
+def mario_pyramid size
+
+	row = "#"
+
+	# Prints row of blocks
+	for block in 0...size
+		puts row
+		row += "#"
+	end
+
+end
+
+mario_pyramid 10
