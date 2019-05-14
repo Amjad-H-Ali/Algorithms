4debb65b1f409bb0794bc778072b6f4296c107a6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-14 18:02:19 -0500

    Implemented mario in reverse

--- mario_pyramid/C++/mario.cpp
+++ mario_pyramid/C++/mario.cpp
@@ -3,20 +3,44 @@
 // Prints Mario Pyramid in given size.
 void marioPyramid(int size);
 
+// Prints Mario Pyramid in reverse
+void marioReversePyramid(int size);
+
 int main() {
 	std::cout << "Mario Pyramid in C++!" << std::endl;
 
 
 	marioPyramid(10);
+	marioReversePyramid(10);
 
 };
 
+// Prints Mario Pyramid in given size.
 void marioPyramid(int size) {
+
 	// Print rows
 	for (int i = 0; i < size; i ++) {
 		// Print columns
-		for (int j = 0; j < i+1; j ++)
+		for (int j = 0, n = i+1; j < n; j ++)
 			std::cout << '#';
-	std::cout << std::endl;
+
+		std::cout << std::endl;
+	}
+};
+
+// Prints Mario Pyramid in reverse
+void marioReversePyramid(int size) {
+	
+	// Print rows
+	for (int i = 0; i < size; i ++) {
+		// Print Columns for spaces
+		for (int j = 0, n = size-(i+1); j < n; j ++)
+			std::cout << ' ';
+		// Print Column for blocks
+		for (int j = 0, n = i+1; j < n; j ++)
+			std:: cout << '#';
+
+		std::cout << std::endl;
+
 	}
 };
