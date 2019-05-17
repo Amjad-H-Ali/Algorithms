0c89e88590ce66b46ea8ec0101b891e58bf90650
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-17 06:45:00 -0500

    Program get CC Num from CLI

--- luhns/Python/luhns.py
+++ luhns/Python/luhns.py
@@ -1,1 +1,26 @@
 print("Luhn's Algorithm in Python!")
+
+def luhns():
+
+	try:
+		ccNum = promptUserForCCNum()
+		print(ccNum)
+	except:
+		luhns()
+
+
+
+
+
+
+# Get Credit Card Number from User
+def promptUserForCCNum():
+
+	# Prompt until valid number is entered
+	while True:
+		print('w')
+		ccNum = input("Enter CC Number: ")
+
+		if(int(ccNum) and len(ccNum) > 1): return ccNum
+
+luhns()
