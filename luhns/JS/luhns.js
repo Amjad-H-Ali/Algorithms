3c88181c91465052f635044aafe7ed9a8381545a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-17 05:12:24 -0500

    Program gets sum of other Numbers + product of every other number

--- luhns/JS/luhns.js
+++ luhns/JS/luhns.js
@@ -5,6 +5,8 @@ console.log("Luhn's Algorithm in JS!");
 	1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products' digits together.
 	2. Add the sum to the sum of the digits that weren’t multiplied by 2.
 	3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+	4. Start: AMEX: 34 or 37, MASTER CARD: [51-55], VISA: 4
+	5. Lenght: AMEX: 15 digits, MASTERCARD: 16 digits, VISA: 13 and 16 digits. 
 */
 
 
@@ -30,6 +32,18 @@ const main = async () => {
 
 	console.log(otherNums);
 
+	let sumOfOtherNumbers = addNumbers(otherNums);
+
+	console.log(sumOfOtherNumbers);
+
+	let sum = sumOfProducts + sumOfOtherNumbers;
+
+	console.log(sum);
+
+	let isValidCCNum = validate(sum);
+
+	console.log(isValidCCNum);
+
 
 	
 }
@@ -44,6 +58,7 @@ const getEverOtherDigit = (numString) => {
 	return [...numString].filter((e, i) => len%2 ? i%2 : !(i%2));
 }
 
+// Returns an array of other numbers 
 const getOtherNumbers = (numString) => {
 	let len = numString.length;
 
@@ -72,11 +87,16 @@ const addProducts = (numArray) => {
 		// Sum up array
 	}).reduce((total, number) => {
 		return total+number;
-	}).toString(); // Convert back to String.
+	});
 }
 
+// Returns Sum of numbers
+const addNumbers = (numArray) => {
+	return numArray.reduce((total, number) => (total-0) + (number-0) // Minus zero Coerces string to int.
+	);
+}
 
-
+// Get CC Num from CLI. Returns Promise
 const getCCNum = () => {
 	return new Promise((resolve) => {
 		prompt.start();
@@ -88,6 +108,15 @@ const getCCNum = () => {
 		
 }
 
+// Validate if credit card number
+const validate = (num) => {
+	// Check if last digit is zero.
+	// Not 0, equals true.
+	// Not 1, equals false.
+	return !(num % 10);
+}
+
+// Keeps Prompting user for CC Num until valid input is entered.
 const promptUserForCCNum = async () => {
 	let ccNum;
 
