34dc4c2304fed661e446b9883637da10dec9255d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-17 11:15:24 -0500

    Deleted print

--- luhns/Python/luhns.py
+++ luhns/Python/luhns.py
@@ -114,7 +114,6 @@ def promptUserForCCNum():
 
 	# Prompt until valid number is entered
 	while True:
-		print('w')
 		ccNum = input("Enter CC Number: ")
 
 		if(int(ccNum) and len(ccNum) > 1): return ccNum
